0f4f22645bdf6d0b25499824bc45d7615073b7d8
Harry Henry Gebel <harry@gebel.tech>
2020-12-28 12:43:23 -0500

    Initial commit

---
+++ app/src/main/java/tech/gebel/tiptime/MainActivity.kt
@@ -0,0 +1,45 @@
+package tech.gebel.tiptime
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import tech.gebel.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        // Extract cost of service or return if invalid
+        val costOfService = binding.costOfServiceText.text.toString().toDoubleOrNull()
+
+        if (costOfService == null) {
+            binding.tipResult.text = ""
+            return
+        }
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        val tip = if (binding.roundUpSwitch.isChecked)
+            kotlin.math.ceil(tipPercentage * costOfService)
+        else
+            tipPercentage * costOfService
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
+
+    }
+}
